5fb7ae9b2852f733c7d30ad1a83f7ed4bad5781b
Ben <benrob99@hotmail.co.uk>
2016-11-19 15:27:00 +0000

    Version 1.0
    -Multiplies a set list by 1.1 and outputs the resulting list

---
+++ Multiply List by 1.1.py
@@ -0,0 +1,12 @@
+def multiply():
+    a=[150,46,69,120,36]
+    print(a)
+    for b in range(len(a)):
+        a[b]*=1.1
+    print(a)
+def multiply2():
+    a=[150,46,69,120,36]
+    print(a)
+    a=[b*1.1 for b in a]
+    print(a)
+multiply2()
