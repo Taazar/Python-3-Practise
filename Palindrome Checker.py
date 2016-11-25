b59eb208a6bfb159de6a527b3fa660d58c3fd017
Ben <benrob99@hotmail.co.uk>
2016-11-25 14:25:32 +0000

    Version 1.3
    +Added line of code to remove all whitespace and punctuation from the input

--- Palindrome Checker.py
+++ Palindrome Checker.py
@@ -1,6 +1,9 @@
+import string
 def palindrome():
     s=input("Please enter the string you would like to test: ")
-    if s[::-1] == s:
+    transtable={ord(c): None for c in string.punctuation}
+    a=s.translate(transtable).lower().replace(' ', '')
+    if a[::-1]==a:
         print("The string '{}' is a palindrome.\n".format(s))
     else:
         print("The string '{}' is not a palindrome.\n".format(s))
