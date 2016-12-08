a89febd84bb50cfcc61faa88fb5f1e088df46c32
Ben <benrob99@hotmail.co.uk>
2016-12-08 18:01:23 +0000

    Version 1.1
    + Changed to sys.exit to quit() removing the import sys 
    + Changed comparing lists in if statements to tuples since they aren't edited
    + Added new line to start of winner outputs
    + Removed unneeded while True loop around first input
    + Changed error message output for move selection
    + Added new dictionary for winning move, eg: smashes, smothers

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,5 +1,4 @@
 from random import choice
-import sys
 import getpass
 
 def player_names(player):
@@ -13,36 +12,34 @@ def game(p1, p2, mode):
     for i in range(mode):
         while True:
             choices = getpass.getpass("\nPlease enter your choice {}: \n1) Rock. \n2) Paper \n3) Scissors \nPlease enter either '1', '2' or '3': ".format(names[i]))
-            if choices not in ["1", "2", "3"]:
-                print("That was not a valid choice.")
+            if choices not in ("1", "2", "3"):
+                print("\nYou did not enter either '1', '2' or '3'.")
                 continue
             else:
                 pc.append(choices)
                 break
     if mode == 1:
-        pc.append(choice(["1", "2", "3"]))
+        pc.append(choice(("1", "2", "3")))
     beats = {"1": "3", "2": "1", "3": "2"}
     objects = {"1": "Rock", "2": "Paper", "3": "Scissors"}
+    moves = {"1": "Smashes", "2": "Smothers", "3": "Cuts"}
     if pc[0] == pc[1]:
-        print("It's a tie! Both {} and {} chose {}.".format(p1, p2, objects[pc[0]]))
+        print("\nIt's a tie! Both {} and {} chose {}.".format(p1, p2, objects[pc[0]]))
     elif beats[pc[0]] == pc[1]:
-        print("{} beats {}! {} wins!".format(objects[pc[0]], objects[pc[1]], p1))
+        print("\n{} {} {}! {} wins!".format(objects[pc[0]], moves[pc[0]], objects[pc[1]], p1))
     else:
-        print("{} beats {}! {} wins!".format(objects[pc[1]], objects[pc[0]], p2))
+        print("\n{} {} {}! {} wins!".format(objects[pc[1]], moves[pc[1]], objects[pc[0]], p2))
 
 def rock_paper_scissors():
     p1, p2, p3 = "Player1", "Player2", "Computer"
     print("Welcome to Rock, Paper, Scissors.")
     while True:
-        while True:
-            choices = input("\nWhat do you want to do? \n1) Start Game \n2) Change Player Names \n3) Quit Game \nPlease enter either '1', '2' or '3': ")
-            if choices not in ["1", "2", "3"]:
-                print("You did not enter either '1', '2' or '3'.")
-                continue
-            else:
-                break
+        choices = input("\nWhat do you want to do? \n1) Start Game \n2) Change Player Names \n3) Quit Game \nPlease enter either '1', '2' or '3': ")
+        if choices not in ("1", "2", "3"):
+            print("\nYou did not enter either '1', '2' or '3'.")
+            continue
         if choices == "3":
-            sys.exit()
+            quit()
         elif choices == "2":
             p1 = player_names(1)
             p2 = player_names(2)
@@ -51,8 +48,8 @@ def rock_paper_scissors():
         else:
             while True:
                 choices = input("\nWho do you want to play against? \n1) Another Person \n2) The Computer \nPlease enter either '1' or '2': ")
-                if choices not in ["1", "2"]:
-                    print("You did not enter either '1' or '2'.")
+                if choices not in ("1", "2"):
+                    print("\nYou did not enter either '1' or '2'.")
                     continue
                 else:
                     break
@@ -63,7 +60,7 @@ def rock_paper_scissors():
         continue
 ##        while True:
 ##            choices = input("Do you want to play again? \n1) Yes \n2) No \nPlease enter either '1' or '2': ")
-##            if choices not in ["1", "2"]:
+##            if choices not in ("1", "2"):
 ##                print("You did not enter either '1' or '2'.")
 ##                continue
 ##            else:
@@ -71,6 +68,6 @@ def rock_paper_scissors():
 ##        if choices2 == "1":
 ##            continue
 ##        else:
-##            sys.exit()
+##            quit()
 
 rock_paper_scissors()
