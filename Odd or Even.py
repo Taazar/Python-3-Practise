141cb01582e321eaac8826ab0a071af6d117c904
Ben <benrob99@hotmail.co.uk>
2016-12-08 17:56:19 +0000

    Version 1.1
    + Changed while a==0 loops to while True loops with breaks

--- Odd or Even.py
+++ Odd or Even.py
@@ -1,19 +1,18 @@
 def odd_even():
-    a=1
-    while a==1:
-        a,b=0,input("Please enter the number you would like to check: ")
+    while True:
+        b=input("Please enter the number you would like to check: ")
         try:
             b=int(b)
+            break
         except:
             print("That was not a number.")
-            a=1
-    while a==0:
-        a,c=1,input("Please enter the number you would like to check it with: ")
+    while True:
+        c=input("Please enter the number you would like to check it with: ")
         try:
             c=int(c)
+            break
         except:
             print("That was not a number.")
-            a=0
     if b%4==0 and b!=0:
         print("The number {} is a multiple of 4.".format(b))
     elif b%2==0:
