f57d879b5e062fad7f5b690b3db9751e10bf3802
Ben <benrob99@hotmail.co.uk>
2016-12-08 17:54:13 +0000

    Version 1.1
    + Changed while a==0 loop to while True loop with a break
    + Changed congrats to congratulations in output message

--- Name Guessing.py
+++ Name Guessing.py
@@ -1,10 +1,10 @@
 def namefinder():
-    name,a="alan",0
-    while a==0:
+    name="alan"
+    while True:
         b=input("Say my name! ")
         if b.lower()==name:
-            print("Congrats, you guessed correctly! ")
-            a=1
+            print("Congratulations, you guessed correctly! ")
+            break
         else:
             print("That's not my name!")
 namefinder()
