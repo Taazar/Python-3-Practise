975c032afa540fa965b1cb29bb3607f297d18b99
Ben <benrob99@hotmail.co.uk>
2016-12-08 17:50:37 +0000

    Version 1.1
    + Replaced while a==1 loops with while True loops with breaks and continues
    + Changed list to a tuple since it isn't changed and tuples are faster

--- List Checker.py
+++ List Checker.py
@@ -1,12 +1,13 @@
 def list_checker():
-    a,b=1,[1,1,2,3,5,8,13,21,34,55,89]
-    while a==1:
-        a,c=0,input("Please enter the number you want all outputs to be smaller than: ")
+    b=(1,1,2,3,5,8,13,21,34,55,89)
+    while True:
+        c=input("Please enter the number you want all outputs to be smaller than: ")
         try:
             c=int(c)
+            break
         except:
             print("That was not a number.")
-            a=1
+            continue
     print("The values less than 5 in the list are {}.".format([d for d in b if d<5]))
     print("The values less than {} in the list are {}.\n".format(c,[d for d in b if d<c]))
     list_checker()
