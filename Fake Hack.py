a6e8603d31cc71b093483de627a8bf10234a759d
Ben <benrob99@hotmail.co.uk>
2016-12-21 21:46:50 +0000

    Version 1.0
    + Uploaded to GitHub

---
+++ Fake Hack.py
@@ -0,0 +1,12 @@
+import time
+print("Hack Loading, please wait...")
+time.sleep(5)
+for a in range(1,11):
+    print("Initialising Backup Servers :{}{}:".format("*"*a," "*(10-a)))
+    time.sleep(0.5)
+print("Accessing Blinded Files")
+time.sleep(0.2)
+print("Unblinding Blinded Files")
+time.sleep(0.2)
+print("Running Administrative Mode")
+time.sleep(1000)
