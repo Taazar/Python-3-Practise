598f1eb199bf612fcc0ada895c284573497e60c6
Ben <benrob99@hotmail.co.uk>
2016-12-21 21:45:23 +0000

    + Version 1.2
    + Removed unneeded continue

--- Divisors of Input.py
+++ Divisors of Input.py
@@ -6,7 +6,6 @@ def divisors():
             break
         except:
             print("That wasn't a number.")
-            continue
     print("Your number is exactly divisible by '{}'.\n".format("', '".join([str(i) for i in range(1,b+1) if b%i==0])))
     divisors()
 divisors()
