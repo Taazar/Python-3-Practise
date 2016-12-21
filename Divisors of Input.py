2f5eea1fa2bb5fc31b50d274675d17b360364be7
Ben <benrob99@hotmail.co.uk>
2016-12-21 22:01:40 +0000

    Version 1.2
    + Fixing version name

--- Divisors of Input.py
+++ Divisors of Input.py
@@ -8,4 +8,4 @@ def divisors():
             print("That wasn't a number.")
     print("Your number is exactly divisible by '{}'.\n".format("', '".join([str(i) for i in range(1,b+1) if b%i==0])))
     divisors()
-divisors()
+divisors() 
