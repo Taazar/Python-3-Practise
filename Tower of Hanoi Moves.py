3f4688ca3f69d5b2b5a2c3858892cd61c37034c3
Taazar <benrob99@hotmail.co.uk>
2019-12-04 12:14:28 +0000

    Improve Readability

--- Tower of Hanoi Moves.py
+++ Tower of Hanoi Moves.py
@@ -1,11 +1,11 @@
 def tower():
     while True:
-        r=input("Please enter the number of rings: ")
+        rings = input("Please enter the number of rings: ")
         try:
-            r=int(r)
+            rings = int(rings)
             break
         except:
             print("That wasn't a number.")
-    print("It will take a minimum of {} moves to complete this tower.\n".format(2**r-1))
+    print("It will take a minimum of {} moves to complete this tower.\n".format((2**rings) - 1))
     tower()
 tower()
