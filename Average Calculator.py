fc7ab07fb95018801ddc11ee4227c1ab7b187d7f
Taazar <benrob99@hotmail.co.uk>
2020-10-27 09:28:09 +0000

    Version 1.6
    Improve readability by using meaningful variable names and spacing

--- Average Calculator.py
+++ Average Calculator.py
@@ -1,45 +1,50 @@
 print("This function will output the mean, median, mode and range of the values you enter.")
 def average():
     while True:
-        a,d=input("Please enter the number of values: "),[]
+        valCount, valList = input("Please enter the number of values: "), []
         try:
-            a=int(a)
-            if a<1:
+            valCount = int(valCount)
+            if valCount < 1:
                 print("You need to have at least one value.")
                 continue
             break
         except ValueError:
             print("You need to input a whole number.")
-    for c in range(a):
+	
+    for i in range(valCount):
         while True:
-            temp=input("Please enter figure number {}: ".format(c+1))
+            temp = input("Please enter figure number {}: ".format(i + 1))
             try:
                 temp=int(temp)
                 break
             except ValueError:
                 print("You need to input a number.")
-        d.append(temp)
-    d.sort()
-    print("Your mean is '{}'.".format((sum(float(c) for c in d))/len(d)))
-    temp=len(d)/2
-    if temp-int(temp)==0:
-        temp=(d[int(temp)]+d[int(temp-1)])/2
+        valList.append(temp)
+    valList.sort()
+    print("Your mean is '{}'.".format((sum(float(i) for i in valList)) / len(valList)))
+	
+    temp = len(valList) / 2
+    if temp - int(temp) == 0:
+        temp = (valList[int(temp)] + valList[int(temp - 1)]) / 2
     else:
-        temp=d[int(temp)]
+        temp = valList[int(temp)]
     print("Your median is '{}'.".format(temp))
-    temp=[d[0]]
-    for c in range(1,a):
-        if d.count(d[c])>d.count(temp[0]):
-            temp=[d[c]]
-        elif d.count(d[c])==d.count(temp[0]) and d[c]!=temp[len(temp)-1]:
-            temp.append(d[c])
-    if len(temp)>1:
-        if len(temp)==a or len(temp)*d.count(temp[0])==a:
+	
+    temp = [valList[0]]
+    for i in range(1, valCount):
+        if valList.count(valList[i]) > valList.count(temp[0]):
+            temp = [valList[i]]
+        elif valList.count(valList[i]) == valList.count(temp[0]) and valList[i] != temp[len(temp) - 1]:
+            temp.append(valList[i])
+	
+    if len(temp) > 1:
+        if len(temp) == valCount or len(temp) * valList.count(temp[0]) == valCount:
             print("There is no real mode, all your values appear an equal amount of times.")
         else:
-            print("Your modes are '{}'.".format("', '".join([str(c) for c in temp])))
+            print("Your modes are '{}'.".format("', '".join([str(i) for i in temp])))
     else:
         print("Your mode is '{}'.".format(temp[0]))
-    print("Your range is '{}'.\n".format(d[a-1]-d[0]))
+    print("Your range is '{}'.\n".format(valList[valCount - 1] - valList[0]))
     average()
+
 average()
