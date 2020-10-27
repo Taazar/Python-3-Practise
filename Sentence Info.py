3886b3f56403d09b721599d0af93071ce44dd53f
Taazar <benrob99@hotmail.co.uk>
2020-10-27 11:21:12 +0000

    Version 1.1
    Improve readability by using meaningful variable names and spacing

--- Sentence Info.py
+++ Sentence Info.py
@@ -1,26 +1,30 @@
 def sentence():
-    a=str(input("What is your sentence? "))
-    b=len(a)
-    c=b-a.count(" ")
-    print("Your sentence has a length of {} characters and spaces.".format(b))
-    print("Your sentence has a length of {} characters.".format(c))
-    print("'{}' is the first half of your sentence.".format(a[:int(b/2)]))
-    print("'{}' is the second half of your sentence.".format(a[int(b/2):]))
-    if"the "in a.lower()or" the "in a.lower()or" the"in a.lower():
+    inText = str(input("What is your sentence? "))
+    lenText = len(inText)
+    charLength = lenText - inText.count(" ")
+	
+    print("Your sentence has a length of {} characters and spaces.".format(lenText))
+    print("Your sentence has a length of {} characters.".format(charLength))
+    print("'{}' is the first half of your sentence.".format(inText[:int(lenText / 2)]))
+    print("'{}' is the second half of your sentence.".format(inText[int(lenText / 2):]))
+	
+    if "the " in inText.lower() or " the " in inText.lower() or " the" in inText.lower():
         print("Your sentence contains the word 'the'.")
     else:
         print("Your sentence does not contain the word 'the'.")
-    if a == a.upper():
+    if inText == inText.upper():
         print("Your sentence contains only uppercase characters.")
         print("Your sentence does not contain only lowercase characters.")
-    elif a == a.lower():
+    elif inText == inText.lower():
         print("Your sentence does not contain only uppercase characters.")
         print("Your sentence contains only lowercase characters.")
     else:
         print("Your sentence does not contain only uppercase characters.")
         print("Your sentence does not contain only lowercase characters.")
-    print("'{}' is your sentence in uppercase characters.".format(a.upper()))
-    print("'{}' is your sentence in lowercase characters.".format(a.lower()))
-    print("'{}' is the unchanged sentence you inputted.\n".format(a))
+	
+    print("'{}' is your sentence in uppercase characters.".format(inText.upper()))
+    print("'{}' is your sentence in lowercase characters.".format(inText.lower()))
+    print("'{}' is the unchanged sentence you inputted.\n".format(inText))
     sentence()
+
 sentence()
